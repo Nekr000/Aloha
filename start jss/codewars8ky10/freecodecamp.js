851a20564fad79e7775a1cdd8d710b6566477fa8
Nekr000 <nekrcode000@gmail.com>
2021-12-12 01:47:14 +0300

    +1

--- start jss/codewars8ky10/freecodecamp.js
+++ start jss/codewars8ky10/freecodecamp.js
@@ -47,7 +47,7 @@ function abTest(a, b) {
 
   return msg;
 
-  // 74% 
+  // 73% 
   const myDog = {
     // Only change code below this line
   name : "Fisher",
@@ -57,4 +57,120 @@ function abTest(a, b) {
   
     // Only change code above this line
   };
-  //75%
+  //74%
+  // Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this lines
+  // 75%
+  // Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
+  //76%
+  // Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+//77%
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  // Only change code below this line
+  myDog.name = "Happy Coder";
+  //78%
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "woof";
+//79%
+  // Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
+
+//80%
+//before
+  switch(val) {
+    case "alpha":
+      result = "Adams";
+      break;
+    case "bravo":
+      result = "Boston";
+      break;
+    case "charlie":
+      result = "Chicago";
+      break;
+    case "delta":
+      result = "Denver";
+      break;
+    case "echo":
+      result = "Easy";
+      break;
+    case "foxtrot":
+      result = "Frank";
+  }
+//after 
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    const lookup = {
+      alpha:"Adams",
+      bravo:"Boston",
+      charlie:"Chicago",
+      delta:"Denver",
+      echo:"Easy",
+      foxtrot:"Frank"
+    };
+    result = lookup[val];
+  
+    // Only change code above this line
+    return result;
+  }
+  // 80% slomannoe pohody =/
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+  
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  
+    // Only change code above this line
+  }
+  
