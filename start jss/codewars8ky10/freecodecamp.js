b2491a67cb52a4df6728962738a893c9c6f501d7
Nekr000 <nekrcode000@gmail.com>
2021-12-12 06:39:44 +0300

    po4ti

--- start jss/codewars8ky10/freecodecamp.js
+++ start jss/codewars8ky10/freecodecamp.js
@@ -376,4 +376,117 @@ do {
 }
 while (i < 10) 
 
-//91%
+//91% ni4ego ne ponyal 
+function sum(arr, n) {
+    // Only change code below this line
+   if (n <= 0) {
+        return 0;
+      } else {
+        return sum(arr, n - 1) + arr[n - 1];
+      }
+    // Only change code above this line
+  }
+  //92%
+
+  // Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No suck property";
+      }
+    }
+  }
+  return "No suck contact";
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Akira", "likes");
+  //93% KOREAN RAANDOM
+  function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
+
+//94%
+function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random() * 10);
+  }
+  //95%
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    var x = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+     x >= myMin;
+     x <= myMax;
+    return x;
+    // Only change code above this line
+  }
+  //96%
+  function convertToInteger(str) {
+    return parseInt(str);
+    }
+    
+    convertToInteger("56");
+    //97%
+    function convertToInteger(str) {
+        const x = parseInt(str, 2);
+        return x;
+        }
+        
+        convertToInteger("10011");
+    //97%
+    function checkEqual(a, b) {
+        return a == b ? "Equal" : "Not Equal";
+        }
+        
+        checkEqual(1, 2);
+    //98%
+    function checkSign(num) {
+        return (num > 0) ? "positive"
+          : (num < 0) ? "negative"
+          : "zero";
+        }
+        
+        checkSign(10);
+    //99% kak 4to kogda
+    function countdown(n){
+        return n < 1 ? [] : [n].concat(countdown(n - 1));
+     }
+     
