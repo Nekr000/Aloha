a5ed5b9b5b24c94180ebc984013ae53f74204e1c
Nekr000 <nekrcode000@gmail.com>
2021-12-12 23:06:19 +0300

    againDefault

---
+++ start jss/codewars8ky10/jsForKids.js
@@ -0,0 +1,37 @@
+//kotiki
+var x = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + "=^_^=");
+    }
+    }
+    x(10);
+
+var secondInMinutes = 60;
+var minutesInHour = 60;
+var secondInHour = minutesInHour * secondInMinutes;
+secondInHour;
+var hourInDay = 24;
+var secondInDay = secondInHour * hourInDay;
+secondInDay;
+var daysInYear = 365;
+var secondsInYear = daysInYear * secondInDay;
+secondsInYear;
+var myAge = 29;
+var secondsInMyLife = myAge * secondsInYear;
+console.log("You Lose", secondsInMyLife);
+
+
+var balloons = 100;
+balloons *= 2;
+
+var balloons = 100;
+balloons /= 4;
+// +=  -= *=  /=
+
+var x = "эЙ, кАК деЛа?";
+var y = x.slice(0,1);
+var a = y.toUpperCase();
+var z = x.slice(1);
+var g = z.toLowerCase();
+a+g;
+
