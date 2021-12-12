99e7dfe84cab2f9d0b6df0d0eeb8992636fc6806
Nekr000 <nekrcode000@gmail.com>
2021-12-12 04:56:14 +0300

    po4ti

--- start jss/codewars8ky10/freecodecamp.js
+++ start jss/codewars8ky10/freecodecamp.js
@@ -308,3 +308,72 @@ const recordCollection = {
     return collection;
   }
   updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+  //85%
+  // Setup
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Only change code below this line
+//86%
+// Setup
+const myArray = [];
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+// Only change code below this line
+//87%
+// Setup
+const myArray = [];
+// Only change code below this line
+for (let i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
+//88%
+// Setup
+const myArray = [];
+for (let i = 9; i>0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line
+//88%
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+// Only change code below this line
+var total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+//89%
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+  for (let i = 0; i < arr.length; i++){
+   for (let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+  }
+      
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+  //90%
+  // Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 10) 
+
+//91%
